7890db9eefe8ef44ebf86dedd9bce58fee9e310c
bcm27 <bcm27@Live.com>
2020-04-10 00:42:17 -0500

    added a basic client description

---
+++ Client/client.py
@@ -0,0 +1,8 @@
+###################################################################################################
+# This python client has three functions
+#   1: clients can connect to a room using a 4 character code shared by the server host
+#   2: clients get assigned cards from the server
+#   3: clients can ask questions to a client in a theoretical clockwise circle
+#   4: clients can guess at the end of each round who the spy is
+#   5: clients can leave the game
+###################################################################################################

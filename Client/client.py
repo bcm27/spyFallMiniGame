e08c0ac3cceb2adc1fbd0bbbc38eed3ce158973d
bcm27 <bcm27@Live.com>
2020-04-11 22:09:55 -0500

    added client layout

--- Client/client.py
+++ Client/client.py
@@ -6,3 +6,14 @@
 #   4: clients can guess at the end of each round who the spy is
 #   5: clients can leave the game
 ###################################################################################################
+
+# launch the gui application 
+
+# connect to the server
+#   1: handle server in coming messages
+#       1: display the incoming card to the player
+#       2: vote on which player to issue the guilty verdict too
+#       3: end game incoming message
+#       4: next round incoming message
+#       5: send room assignment to server
+
